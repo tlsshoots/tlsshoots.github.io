f6eda6b58951c7a2a633c81f8dcfca15b15f1a66
wvicioso <wellington.vicioso@gmail.com>
2019-01-07 21:49:38 -0500

    Updates

---
+++ precache-manifest.681130e43f8d8826fc3e867d35c31e7f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "ba93efd3e8e5bb8c72498eb0e1ac4c40",
+    "url": "/static/media/tls-camera.ba93efd3.jpg"
+  },
+  {
+    "revision": "d2b3583ce669f828404669c5e6cb21e7",
+    "url": "/static/media/private.d2b3583c.jpg"
+  },
+  {
+    "revision": "0f9e552836cf4a6307c584287e7ab8d3",
+    "url": "/static/media/TLSLogo.0f9e5528.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8e736ef07281abf040ae",
+    "url": "/static/js/main.8e736ef0.chunk.js"
+  },
+  {
+    "revision": "b17e660d3dd28a355550",
+    "url": "/static/js/1.b17e660d.chunk.js"
+  },
+  {
+    "revision": "8e736ef07281abf040ae",
+    "url": "/static/css/main.ef12dc59.chunk.css"
+  },
+  {
+    "revision": "b17e660d3dd28a355550",
+    "url": "/static/css/1.cf4ac1da.chunk.css"
+  },
+  {
+    "revision": "e9592527578a4d2bda74263748233a52",
+    "url": "/index.html"
+  }
+];
